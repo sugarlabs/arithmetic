sugarlabs/arithmetic

commit e500c592b80e761aea491cf675c0112749a42236

Port dobject to Gtk3

dobject/groupthink/sugar_tools.py
@@ -5,20 +5,21 @@ a module to make sharing easier in Sugar Activities.
 import logging
 import telepathy
 
-from sugar.activity.activity import Activity, ActivityToolbox
-from sugar.presence import presenceservice
+from sugar3.activity.activity import Activity
+from sugar3.activity.widgets import ActivityToolbar
+from sugar3.presence import presenceservice
 
-from sugar.presence.tubeconn import TubeConnection
-from sugar.graphics.window import Window
+from sugar3.presence.tubeconn import TubeConnection
+from sugar3.graphics.window import Window
 
-import gtk
+from gi.repository import Gtk,Gdk
 import gobject
 
 from dobject.groupthink import groupthink_base as groupthink
 
 def exhaust_event_loop():
-    while gtk.events_pending():
-        gtk.main_iteration()
+    while Gtk.events_pending():
+        Gtk.main_iteration()
 
 class GroupActivity(Activity):
     """
@@ -80,12 +81,12 @@ class GroupActivity(Activity):
             self.message = self.message_preparing
 
         # top toolbar with share and close buttons:
-        toolbox = ActivityToolbox(self)
+        toolbox = ActivityToolbar(self)
         self.set_toolbox(toolbox)
         toolbox.show()
         
-        v = gtk.VBox()
-        self.startup_label = gtk.Label(self.message)
+        v = Gtk.VBox()
+        self.startup_label = Gtk.Label(self.message)
         v.pack_start(self.startup_label)
         Window.set_canvas(self,v)
         self.show_all()
@@ -122,7 +123,7 @@ class GroupActivity(Activity):
             else:
                 self._joined_cb(self)
         
-        self.add_events(gtk.gdk.VISIBILITY_NOTIFY_MASK)
+        self.add_events(Gdk.EventMask.VISIBILITY_NOTIFY_MASK)
         self.connect("visibility-notify-event", self._visible_cb)
         self.connect("notify::active", self._active_cb)
         
@@ -312,7 +313,7 @@ class GroupActivity(Activity):
             
     def _visible_cb(self, widget, event):
         self.logger.debug("_visible_cb")
-        if event.state == gtk.gdk.VISIBILITY_FULLY_OBSCURED:
+        if event.state == Gdk.VisibilityState.FULLY_OBSCURED:
             self.pause()
         else:
             self.resume()

dobject/demo_activities/SharedTextDemo.activity/sharedtextdemoactivity.py
@@ -1,9 +1,10 @@
 from dobject.groupthink import sugar_tools, gtk_tools
-import sugar
-import gtk
+import sugar3
+from gi.repository import Gtk
+
 
 class SharedTextDemoActivity(sugar_tools.GroupActivity):
     def initialize_display(self):
         self.cloud.textview = gtk_tools.SharedTextView()
-        self.cloud.textview.props.wrap_mode = gtk.WRAP_WORD
+        self.cloud.textview.props.wrap_mode = Gtk.WrapMode.WORD
         return self.cloud.textview

dobject/groupthink/groupthink_base.py
@@ -1444,7 +1444,7 @@ class CausalDict(CausalObject):
             n = self.handler.send(dbus.Array([(dbus.Int32(CausalDict.ADD), self._key_trans(key, True), self._val_trans(value, True))]))
         self._index_dict[key] = n
     
-    def update(*args,**kargs):
+    def update(self, *args,**kargs):
         """Same as for dict"""
         d = dict()
         d.update(*args,**kargs)

dobject/groupthink/gtk_tools.py
@@ -8,12 +8,13 @@ helper classes to allow you to build your own shared widgets.
 Most of the classes here are L{HandlerAcceptor}s, meaning that they can easily
 be shared by assigning them to a properly configured L{Group}.
 """
-import gtk
+from gi.repository import Gtk,Gdk
 from dobject.groupthink import groupthink_base as groupthink
 import logging
 from dobject.groupthink import stringtree
 
-class RecentEntry(groupthink.UnorderedHandlerAcceptor, gtk.Entry):
+
+class RecentEntry(groupthink.UnorderedHandlerAcceptor, Gtk.Entry):
     """
     RecentEntry is an extension of gtk.Entry that, when attached to a group,
     creates a unified Entry field for all participants.  It is implemented using
@@ -23,9 +24,9 @@ class RecentEntry(groupthink.UnorderedHandlerAcceptor, gtk.Entry):
         """
         All arguments to __init__ are passed directly to gtk.Entry.__init__
         """
-        gtk.Entry.__init__(self, *args, **kargs)
+        Gtk.Entry.__init__(self, *args, **kargs)
         self.logger = logging.getLogger('RecentEntry')
-        self.add_events(gtk.gdk.PROPERTY_CHANGE_MASK)
+        self.add_events(Gdk.EventMask.PROPERTY_CHANGE_MASK)
         self._text_changed_handler = self.connect('changed', self._local_change_cb)
         self._recent = groupthink.Recentest(self.get_text(), groupthink.string_translator)
         self._recent.register_listener(self._remote_change_cb)
@@ -48,16 +49,17 @@ class RecentEntry(groupthink.UnorderedHandlerAcceptor, gtk.Entry):
             self.set_text(text)
             self.handler_unblock(self._text_changed_handler)
 
-class SharedToggleButton(groupthink.UnorderedHandlerAcceptor, gtk.ToggleButton):
+
+class SharedToggleButton(groupthink.UnorderedHandlerAcceptor, Gtk.ToggleButton):
     """SharedToggleButton is an extension of gtk.ToggleButton that, when attached
      to a group, creates a unified ToggleButton for all participants."""
     def __init__(self, *args, **kargs):
         """
         All arguments to __init__ are passed directly to gtk.ToggleButton.__init__
         """
-        gtk.ToggleButton.__init__(self, *args, **kargs)
+        Gtk.ToggleButton.__init__(self, *args, **kargs)
         self.logger = logging.getLogger('SharedToggleButton')
-        self.add_events(gtk.gdk.PROPERTY_CHANGE_MASK)
+        self.add_events(Gdk.EventMask.PROPERTY_CHANGE_MASK)
         self._change_handler = self.connect('toggled', self._local_change_cb)
         self._recent = groupthink.Recentest(self.get_active(), groupthink.bool_translator)
         self._recent.register_listener(self._remote_change_cb)
@@ -81,7 +83,7 @@ class SharedToggleButton(groupthink.UnorderedHandlerAcceptor, gtk.ToggleButton):
             self.handler_unblock(self._change_handler)
 
 
-class SharedTreeStore(groupthink.CausalHandlerAcceptor, gtk.GenericTreeModel):
+class SharedTreeStore(groupthink.CausalHandlerAcceptor, Gtk.GenericTreeModel):
     """
     WARNING: SharedTreeStore is UNTESTED and probably does not work.
     
@@ -133,7 +135,7 @@ class SharedTreeStore(groupthink.CausalHandlerAcceptor, gtk.GenericTreeModel):
     ### Methods necessary to implement gtk.GenericTreeModel ###
 
     def on_get_flags(self):
-        return gtk.TREE_MODEL_ITERS_PERSIST
+        return Gtk.TreeModelFlags.ITERS_PERSIST
 
     def on_get_n_columns(self):
         return len(self._columntypes)
@@ -355,6 +357,7 @@ class SharedTreeStore(groupthink.CausalHandlerAcceptor, gtk.GenericTreeModel):
         p = self.get_user_data(newparent)
         self._causaltree.change_parent(node,p)
 
+
 class TextBufferUnorderedStringLinker:
     """
     Translates between a gtk.TextBuffer and a L{UnorderedString}.  This object
@@ -424,7 +427,8 @@ class TextBufferSharePoint(groupthink.UnorderedHandlerAcceptor):
     def set_handler(self, handler):
         self._us.set_handler(handler)
 
-class SharedTextView(groupthink.UnorderedHandlerAcceptor, gtk.TextView):
+
+class SharedTextView(groupthink.UnorderedHandlerAcceptor, Gtk.TextView):
     """
     SharedTextView is a network-shareable version of gtk.TextView.
     It provides a simple wrapper around L{TextBufferSharePoint}.
@@ -436,7 +440,7 @@ class SharedTextView(groupthink.UnorderedHandlerAcceptor, gtk.TextView):
         """
         All argument are passed directly to gtk.TextView.__init__
         """
-        gtk.TextView.__init__(self, *args, **kargs)
+        Gtk.TextView.__init__(self, *args, **kargs)
         self._link = TextBufferSharePoint(self.get_buffer())
         
     def set_handler(self, handler):

dobject/groupthink/listset.py
@@ -484,7 +484,7 @@ class ListDict:
     def __hash__(self):
         return self._dict.__hash__()
     
-    def __iter__():
+    def __iter__(self):
         return self.iterkeys()
     
     def __le__(self, d):

dobject/listset_test.py
@@ -1,4 +1,4 @@
-from .listset import *
+from dobject.groupthink.listset import *
 from random import random
 
 class SquareCompare:

commit b81f74e66abfea269707d29ce6ff36e60def12c7

Fix Port to Gtk3 regression
Introduced in https://github.com/sugarlabs/arithmetic/pull/7/commits/e500c592b80e761aea491cf675c0112749a42236

dobject/groupthink/sugar_tools.py
@@ -8,7 +8,7 @@ import logging
 from gi.repository import TelepathyGLib
 
 from sugar3.activity.activity import Activity
-from sugar3.activity.widgets import ActivityToolbar
+from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.presence import presenceservice
 
 from sugar3.presence.tubeconn import TubeConnection
@@ -85,8 +85,8 @@ class GroupActivity(Activity):
             self.message = self.message_preparing
 
         # top toolbar with share and close buttons:
-        toolbox = ActivityToolbar(self)
-        self.set_toolbox(toolbox)
+        toolbox = ToolbarBox(self)
+        self.set_toolbar_box(toolbox)
         toolbox.show()
 
         v = Gtk.VBox()